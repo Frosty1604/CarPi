2cb527a37ce308e1b30d26cfc62a8f9c8806c026
yse <yse@ixenso.com>
2019-07-17 09:50:52 +0200

    Cleaned up

--- main.py
+++ main.py
@@ -8,33 +8,6 @@ from reader import Reader
 
 connection = obd.OBD("/dev/ttys001")
 
-# response_SPEED = connection.query(obd.commands.SPEED)
-# response_RUN_TIME = connection.query(obd.commands.RUN_TIME)
-# response_RPM = connection.query(obd.commands.RPM)
-# response_THROTTLE_POS = connection.query(obd.commands.THROTTLE_POS)
-# response_FUEL_TYPE = connection.query(obd.commands.FUEL_TYPE)
-# response_FUEL_RATE = connection.query(obd.commands.FUEL_RATE)
-
-# while True:
-#     response_SPEED = connection.query(obd.commands.SPEED)
-#     response_RUN_TIME = connection.query(obd.commands.RUN_TIME)
-#     response_RPM = connection.query(obd.commands.RPM)
-#     response_THROTTLE_POS = connection.query(obd.commands.THROTTLE_POS)
-#     response_FUEL_TYPE = connection.query(obd.commands.FUEL_TYPE)
-#     response_DTC = connection.query(obd.commands.GET_DTC)
-#
-#     print("------------------------")
-#     print
-#     print(response_SPEED)
-#     print(response_RUN_TIME)
-#     print(response_THROTTLE_POS)
-#     print(response_RPM)
-#     print(response_FUEL_TYPE)
-#     print(response_DTC)
-#     print()
-#     print("------------------------")
-#     time.sleep(1)
-
 reader = Reader("/dev/ttys002", "Test")
 
 
@@ -49,11 +22,9 @@ def get_data():
 
 
 print(connection.status())
-# print(obd.commands.SPEED)
 
 if __name__ == '__main__':
     reader.init_logger()
     while True:
         reader.logger()
         time.sleep(1)
-# get_data()
